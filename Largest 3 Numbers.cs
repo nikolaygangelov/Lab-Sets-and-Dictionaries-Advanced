f902253db1c44456798969b89d0e12bb123bed10
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-01-24 13:44:35 +0200

    Add files via upload

---
+++ Largest 3 Numbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            numbers = numbers.OrderByDescending(n => n)
+                .ToArray();
+            if (numbers.Length < 3)
+            {
+                Console.WriteLine(string.Join(' ', numbers));
+            }
+            else
+            {
+                Console.WriteLine(string.Join(' ', numbers.Take(3)));
+            }
+        }
+    }
+}
