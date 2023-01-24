1c10fa50a782b1faf8ec1c1501e1970b408300ca
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-01-24 14:35:28 +0200

    Add files via upload

---
+++ Record Unique Names.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6._Record_Unique_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            int numberOfNames = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfNames; i++)
+            {
+                set.Add(Console.ReadLine());
+            }
+            Console.WriteLine("-------------");
+            foreach (string name in set)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
